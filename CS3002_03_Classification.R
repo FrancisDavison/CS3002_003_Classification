010ba3e2cedd0802713fb83e17ebe1d70851bb46
FrancisDavison <francis.nj.davison@gmail.com>
2021-10-16 19:37:07 +0100

    started lab

---
+++ CS3002_03_Classification.R
@@ -0,0 +1,18 @@
+winedata = read.csv('C:\\Users\\Picard\\Dropbox\\Github\\CS3002_003_Classification\\winedata3.csv', sep=",")
+
+wineclass = winedata[,1]
+winevalues = winedata[,-1]
+
+#set up training set
+wineclassTrain = wineclass[1:100]
+winevaluesTrain = winedata[1:100,]
+
+#and test set
+wineclassTest = wineclass[100:178]
+winevaluesTest = winevalues[100:178,]
+
+library(rpart)
+fit <- rpart(wineclassTrain~., method="class", data=winevaluesTrain)
+
+plot(fit, uniform=TRUE, main="Decision Treee for WineData3")
+text(fit, use.n=TRUE, all=TRUE, cex=.8)
